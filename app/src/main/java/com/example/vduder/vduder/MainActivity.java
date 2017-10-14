eb637f476bc4b5428f011d4147ef175d52b1d379
Dmitry Yunkov <dima.yonkov@gmail.com>
2017-10-14 09:20:46 +0300

    first commit

---
+++ app/src/main/java/com/example/vduder/vduder/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.vduder.vduder;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.StyleableRes;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private DatabaseReference mDatabase;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        mAuth = FirebaseAuth.getInstance();
+//        mDatabase = FirebaseDatabase.getInstance().getReference();
+//        mAuth.signInWithEmailAndPassword("dima.yonkov@gmail.com", "11111111")
+//                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<AuthResult> task) {
+//                        if (task.isSuccessful()) {
+//                            // Sign in success, update UI with the signed-in user's information
+//                            Log.d("dd", "signInWithEmail:success");
+//                            FirebaseUser user = mAuth.getCurrentUser();
+//                            Toast.makeText(MainActivity.this, "МЫ ЗАШЛИ СУКААА",
+//                                    Toast.LENGTH_SHORT).show();
+//                            writeNewUser("Dmitry", "dima.yonkov@gmail.com");
+//                        } else {
+//                            // If sign in fails, display a message to the user.
+//                            Log.w("dd", "signInWithEmail:failure", task.getException());
+//                            Toast.makeText(MainActivity.this, "Authentication failed.",
+//                                    Toast.LENGTH_SHORT).show();
+//                        }
+//                        // ...
+//                    }
+//                });
+        }
+//
+//    private void writeNewUser(String name, String email) {
+//        User user = new User(name, email);
+//
+//        mDatabase.child("users").child().setValue(user);
+//    }
+
+}
---
+++ app/src/main/java/com/example/vduder/vduder/User.java
@@ -0,0 +1,24 @@
+package com.example.vduder.vduder;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+/**
+ * Created by dmitry on 13.10.17.
+ */
+
+@IgnoreExtraProperties
+public class User {
+
+    public String username;
+    public String email;
+
+    public User() {
+        // Default constructor required for calls to DataSnapshot.getValue(User.class)
+    }
+
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+}
